37e38b936cb4bd4a966ae055963a06b967ec3672
jana <janamck007@gmail.com>
2017-04-01 15:59:51 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# push-it-bot
-slack bot for yer git pushes
+# push it bot
+slack bot for them there github projects. Plays 'ah push it' when user pushes to monitored repo.
+
+export SLACK_BOT_TOKEN='api key'
+export USER_NAME='name'
+export BOT_ID=$(python print_bot_id.py) 
---
+++ pushit.py
@@ -0,0 +1,24 @@
+import os
+import time
+from slackclient import SlackClient
+
+BOT_ID = os.environ.get("BOT_ID")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+f __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("Pushit connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
